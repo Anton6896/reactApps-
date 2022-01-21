73af6d17f4cc32ab8bd6b61facdb8b21520c71e6
ant <anton-r@live.com>
2022-01-21 08:31:00 +0200

    reformating structure

---
+++ github_finder_v2/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import React from 'react';
+import Navbar from './componentrs/layout/Navbar';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <h2> anton testing</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ github_finder_v2/src/componentrs/layout/Navbar.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class Navbar extends Component {
+  render() {
+    return <div>
+        <h4> navbar component</h4>
+    </div>;
+  }
+}
+
+export default Navbar;
