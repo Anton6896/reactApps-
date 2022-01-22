bb4434c9f5ca9baf9def533c1eb73affd334d14f
anton-r <anton-r@live.com>
2022-01-22 09:05:35 +0200

    getting user from github api

--- github_finder_v2/src/App.jsx
+++ github_finder_v2/src/App.jsx
@@ -2,37 +2,50 @@ import './App.css';
 import React from 'react';
 import NavbarLayout from './components/layout/NavbarLayout';
 import Users from './components/users/users';
-import { Container, Spinner } from 'react-bootstrap';
+import {Container, Spinner} from 'react-bootstrap';
 
 
 class App extends React.Component {
 
     state = {
         users: [],
-        loading: false
+        loading: false,
+        searchData: '',
     }
 
-    async componentDidMount() {
-        this.setState({ loading: true })
-        
+    componentDidMount = async () => {
+        this.setState({loading: true})
+
         const url = `https://api.github.com/users?per_page=10&
         client_id=${process.env.TOKEN}&
         client_secret=${process.env.PASSWORD}`
-
         let res = await fetch(url)
         let data = await res.json()
 
-        this.setState({ users: data })
-        this.setState({ loading: false })
+        this.setState({users: data})
+        this.setState({loading: false})
+    }
+
+    searchUser = async (name) => {
+        this.setState({loading: true})
+
+        const url = `https://api.github.com/search/users?q=${name}&
+        client_id=${process.env.TOKEN}&
+        client_secret=${process.env.PASSWORD}`
+        let res = await fetch(url)
+        let data = await res.json()
+
+        this.setState({users: data.items})
+        this.setState({loading: false})
     }
 
     render() {
         return (
             <div>
-                <NavbarLayout title={'myapp'} />
+                <NavbarLayout title={'myapp'}
+                              searchUser={this.searchUser}/>
 
-                <Container style={{ margin: '30px 0 0 0' }}>
-
+                <Container style={{margin: '30px 0 0 0'}}>
                     {
 
                         this.state.loading ?
@@ -40,9 +53,8 @@ class App extends React.Component {
                                 <span className="visually-hidden">Loading...</span>
                             </Spinner>
                             :
-                            <Users users={this.state.users} />
+                            <Users users={this.state.users}/>
                     }
-
 
                 </Container>
             </div>
--- github_finder_v2/src/components/layout/NavbarLayout.jsx
+++ github_finder_v2/src/components/layout/NavbarLayout.jsx
@@ -1,46 +1,64 @@
-import React, { Component } from 'react';
-import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap'
+import React, {Component} from 'react';
+import {Navbar, Nav, NavDropdown, Form, FormControl, Button} from 'react-bootstrap'
 import PropTypes from 'prop-types'
 
-const NavbarLayout = ({ title }) => {
-    return (
-        <Navbar sticky="top" bg="light" expand="lg">
-            <Navbar.Brand style={{ margin: '0 0 0 15px' }} href="#home">{title}</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#link">Link</Nav.Link>
-                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                    </NavDropdown>
-                </Nav>
+// class NavbarLayout = ({title, searchData, onSearch}) => {
+class NavbarLayout extends Component {
+    static defaultProps = { // auto load
+        title: 'default title'
+    }
 
-                <Form className="d-flex">
-                    <FormControl
-                        type="search"
-                        placeholder="Search"
-                        className="me-2"
-                        aria-label="Search"
-                    />
-                    <Button variant="outline-success"
-                        style={{ margin: '0 50px 0 0' }}>Search</Button>
-                </Form>
-            </Navbar.Collapse>
-        </Navbar>
-    )
+    state = {
+        search: ''
+    }
+
+    onchange = (e) => {
+        this.setState({[e.target.name]: e.target.value})
+    }
+
+    onSubmit = (e) =>{
+        e.preventDefault()
+        this.props.searchUser(this.state.search)
+    }
+
+    render() {
+        let {title} = this.props
+
+        return (
+            <Navbar sticky="top" bg="light" expand="lg">
+                <Navbar.Brand style={{margin: '0 0 0 15px'}} href="#home">{title}</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#link">Link</Nav.Link>
+                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Divider/>
+                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+
+                    <Form className="d-flex" onSubmit={this.onSubmit}>
+                        <FormControl
+                            type="search"
+                            name={"search"}
+                            placeholder="Search"
+                            className="me-2"
+                            onChange={this.onchange}
+                            aria-label="Search"
+                        />
+                        <Button variant="outline-success"
+                                type={"submit"}
+                                style={{margin: '0 50px 0 0'}}>Search</Button>
+                    </Form>
+                </Navbar.Collapse>
+            </Navbar>
+        )
+    }
 }
 
-NavbarLayout.defaultProps = { // auto load 
-    title: 'default title'
-}
-
-NavbarLayout.propTypes = {
-    title: PropTypes.string.isRequired // check string 
-}
 
 export default NavbarLayout;
