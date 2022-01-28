3e81b12dbff5c55587300d5637bda5c33b2c0254
anton-r <anton-r@live.com>
2022-01-28 16:13:19 +0200

    table added

--- github_finder_v2/src/components/users/UserProfile.jsx
+++ github_finder_v2/src/components/users/UserProfile.jsx
@@ -1,44 +1,37 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import React, {useState, useEffect} from 'react';
+import {useParams} from 'react-router-dom';
 import Table from 'react-bootstrap/Table'
-import { Container } from 'react-bootstrap';
+import {Container} from 'react-bootstrap';
 
 
-export default function UserProfile({ getUserProfile, user }) {
-    let { username } = useParams()
+export default function UserProfile({getUserProfile, user}) {
+    let {username} = useParams()
 
     useEffect(() => {
         getUserProfile(username)
-    }, []);
+    }, [getUserProfile, username]);
 
     return (
         <Table striped bordered hover>
             <thead>
-                <tr>
-                    <th>#</th>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Username</th>
-                </tr>
+            <tr>
+                <th>#</th>
+                <th>key</th>
+                <th>value</th>
+            </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                </tr>
-                <tr>
-                    <td>2</td>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                </tr>
-                <tr>
-                    <td>3</td>
-                    <td colSpan={2}>Larry the Bird</td>
-                    <td>@twitter</td>
-                </tr>
+            {
+                Object.keys(user).map((key, i) => {
+                    return (
+                        <tr key={i}>
+                            <td>{i}</td>
+                            <td>{key}</td>
+                            <td>{user[key]}</td>
+                        </tr>
+                    )
+                })
+            }
             </tbody>
         </Table>
     );
