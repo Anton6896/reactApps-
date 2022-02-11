96977514425f5008a16997702b6359e852d9d1e3
anton-r <anton-r@live.com>
2022-02-11 10:23:39 +0200

    add materialize and icons to project

--- newapp/README.md
+++ newapp/README.md
@@ -1,3 +1,7 @@
 cource code : https://github.com/bradtraversy/it-logger 
 json server : https://github.com/typicode/json-server
 materialize to react : https://materializecss.com/
+
+some other style
+nui : https://mui.com/getting-started/usage/   
+icons : https://mui.com/getting-started/installation/
--- newapp/src/App.jsx
+++ newapp/src/App.jsx
@@ -1,10 +1,21 @@
-import React, {useContext} from 'react';
+import React, { useEffect } from 'react';
+import M from 'materialize-css/dist/js/materialize.min.js'
+import 'materialize-css/dist/css/materialize.min.css'
 
 export default function App() {
 
-    return (
-        <div>
-          <p>testing</p>
-        </div>
-    );
+  useEffect(() => { 
+    M.AutoInit()  // init materialize JS  
+  }, [])
+
+  return (
+    <div>
+      <p>testing</p>
+      <div>
+        <p>
+          <a className="waves-effect waves-light btn">button</a>
+        </p>
+      </div>
+    </div>
+  );
 }
