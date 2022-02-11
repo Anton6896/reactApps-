5523e4e07e55d4c1495b2525f75860a59539eefc
anton-r <anton-r@live.com>
2022-02-11 15:13:35 +0200

    logs added

--- newapp/src/App.jsx
+++ newapp/src/App.jsx
@@ -3,6 +3,10 @@ import React, { useEffect } from 'react';
 import M from 'materialize-css/dist/js/materialize.min.js'
 import 'materialize-css/dist/css/materialize.min.css'
 
+import NavbarComponent from './components/NavBar/navbarComponent';
+import SearchBar from './components/NavBar/searchBar';
+import Logs from './components/logs/Logs';
+
 export default function App() {
 
   useEffect(() => {
@@ -11,27 +15,11 @@ export default function App() {
 
   return (
     <div>
-      <nav style={{ marginButtom: '30px' }} className='#ba68c8 purple lighten-2'>
-        <div className="nav-wrapper"  >
-          <a href="#!" style={{marginLeft: '30px'}} className="brand-logo">Logo</a>
-          <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
-          <ul className="right hide-on-med-and-down">
-            {/* // on nav links */}
-            <li><a href="sass.html">Sass</a></li>
-            <li><a href="badges.html">Components</a></li>
-            <li><a href="collapsible.html">Javascript</a></li>
-            <li><a href="mobile.html">Mobile</a></li>
-          </ul>
-        </div>
-      </nav>
+      <NavbarComponent />
 
-      {/* // mobile links */}
-      <ul className="sidenav" id="mobile-demo">
-        <li><a href="sass.html">Sass</a></li>
-        <li><a href="badges.html">Components</a></li>
-        <li><a href="collapsible.html">Javascript</a></li>
-      </ul>
-
+      <div className='container'>
+        <Logs />
+      </div>
 
     </div>
   );
