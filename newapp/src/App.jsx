8759b46e85b3cfc32015eaeb107621e1ce1079a7
anton-r <anton-r@live.com>
2022-02-11 10:52:19 +0200

    basicks ok

--- newapp/src/App.jsx
+++ newapp/src/App.jsx
@@ -1,21 +1,38 @@
 import React, { useEffect } from 'react';
+
 import M from 'materialize-css/dist/js/materialize.min.js'
 import 'materialize-css/dist/css/materialize.min.css'
 
 export default function App() {
 
-  useEffect(() => { 
+  useEffect(() => {
     M.AutoInit()  // init materialize JS  
   }, [])
 
   return (
     <div>
-      <p>testing</p>
-      <div>
-        <p>
-          <a className="waves-effect waves-light btn">button</a>
-        </p>
-      </div>
+      <nav style={{ marginButtom: '30px' }} className='#ba68c8 purple lighten-2'>
+        <div className="nav-wrapper"  >
+          <a href="#!" style={{marginLeft: '30px'}} className="brand-logo">Logo</a>
+          <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+          <ul className="right hide-on-med-and-down">
+            {/* // on nav links */}
+            <li><a href="sass.html">Sass</a></li>
+            <li><a href="badges.html">Components</a></li>
+            <li><a href="collapsible.html">Javascript</a></li>
+            <li><a href="mobile.html">Mobile</a></li>
+          </ul>
+        </div>
+      </nav>
+
+      {/* // mobile links */}
+      <ul className="sidenav" id="mobile-demo">
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li><a href="collapsible.html">Javascript</a></li>
+      </ul>
+
+
     </div>
   );
 }
