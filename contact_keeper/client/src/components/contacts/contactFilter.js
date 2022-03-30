a3f1317e0424c9bb540683bfbd726ffd936deace
anton-r <anton-r@live.com>
2022-03-30 20:56:31 +0300

    ff

--- contact_keeper/client/src/components/contacts/contactFilter.js
+++ contact_keeper/client/src/components/contacts/contactFilter.js
@@ -1,34 +1,40 @@
-import React, {useContext, useState} from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import ContactContext from "../../contex/contact/ContactContext";
 
 let ContactFilter = () => {
 
     let context = useContext(ContactContext)
-    let [text, setText] = useState('')
+    const [text, setText] = useState('')
+
 
 
     return (
         <form onSubmit={(e) => {
             e.preventDefault()
         }}>
-            <input type="text" placeholder={'filter'} value={text}
-                   onChange={e => {
-                       if (e.target.value) {
-                           setText(e.target.value);
-                           context.filterContacts(text)
-                       } else {
-                           context.clearFilterContacts()
-                       }
-                   }}
+            <input type="text"
+                placeholder={'filter'}
+                value={text}
+
+            onChange={e => {
+                if (e.target.value) {
+                    console.log(e.target.value)
+                    setText(e.target.value);
+                    context.filterContacts(e.target.value)
+
+                } else {
+                    context.clearFilterContacts()
+                }
+            }}
             />
 
             {
                 (text !== '') &&
                 <button className="btn btn-light btn-block"
-                        onClick={() => {
-                            context.clearFilterContacts()
-                            setText('')
-                        }}>clear</button>
+                    onClick={() => {
+                        context.clearFilterContacts()
+                        setText('')
+                    }}>clear</button>
             }
         </form>
     );
