d4b49e6654c1edf2f7800bff6bd0347a4871fa18
Anton.R <Anton-R@live.com>
2023-01-07 11:48:06 +0200

    nav bar ok

--- react_typeScript_app_1/app/src/App.tsx
+++ react_typeScript_app_1/app/src/App.tsx
@@ -1,12 +1,24 @@
 import "./App.css";
-import Button from '@mui/material/Button';
+import { Routes, Route } from "react-router-dom";
+
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+
+import { About } from "./pages/About";
+import { Store } from "./pages/Store";
+import { Home } from "./pages/Home";
+import { AppNavbar } from "./components/Navbar";
 
 function App() {
   return (
-    <div>
-      <h1> hello </h1>
-      <Button variant="contained">Contained</Button>
-    </div>
+    <Container>
+      <AppNavbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Container>
   );
 }
 
---
+++ react_typeScript_app_1/app/src/components/Navbar.tsx
@@ -0,0 +1,41 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import { Link, matchPath, useLocation } from "react-router-dom";
+
+// https://mui.com/material-ui/guides/routing/#:~:text=))%3B-,Tabs,-Current%20route%3A
+function useRouteMatch(patterns: readonly string[]) {
+  const { pathname } = useLocation();
+
+  for (let i = 0; i < patterns.length; i += 1) {
+    const pattern = patterns[i];
+    const possibleMatch = matchPath(pattern, pathname);
+    if (possibleMatch !== null) {
+      return possibleMatch;
+    }
+  }
+
+  return null;
+}
+
+function MyTabs() {
+  const routeMatch = useRouteMatch(["/", "/store", "/about"]);
+  const currentTab = routeMatch?.pattern?.path;
+
+  return (
+    <Tabs value={currentTab}>
+      <Tab label="Home" value="/" to="/" component={Link} />
+      <Tab label="Store" value="/store" to="/store" component={Link} />
+      <Tab label="About" value="/about" to="/about" component={Link} />
+    </Tabs>
+  );
+}
+
+export function AppNavbar() {
+  return (
+    <Box sx={{ width: "100%" }}>
+      <MyTabs />
+    </Box>
+  );
+}
